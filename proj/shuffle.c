cb98be3e6605a5167b092cfc78f7a7d129651b50
synpse <tfa@sapo.pt>
2018-01-28 01:56:38 +0000

    Add all files
    Signed-off-by: synpse <tfa@sapo.pt>

---
+++ proj/shuffle.c
@@ -0,0 +1,10 @@
+#include "showworld.h"
+#include "shuffle.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+void Shuffle(int AGENT *agents, unsigned int nagents) { //implementation of shuffle
+int newShuffle[];
+int ag_contador = 0;
+		}
---
+++ proj/shuffle.h
@@ -0,0 +1,11 @@
+#include "showworld.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+void shuffle(AGENT *agents, unsigned int nagents) {
+	
+	
+	
+	
+}
