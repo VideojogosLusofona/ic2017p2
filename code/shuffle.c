698257aab6926e404fe14a76c530b967f7a2cb3c
synpse <tfa@sapo.pt>
2018-01-30 21:36:07 +0000

    Add shuffle
    Signed-off-by: synpse <tfa@sapo.pt>

---
+++ code/shuffle.c
@@ -0,0 +1,18 @@
+#include "showworld.h"
+#include "shuffle.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/*void Shuffle(AGENT *agents, unsigned int nagents) { //implementation of Fisher
+AGENT tmp;
+int i, j;
+  
+  for (i = nagents - 1; i > 0 1; i--) {
+    j = rand() % (i + 1);
+    tmp = agents[j];
+    array[j] = array[i];
+    array[i] = tmp;
+  }
+}*/
+
---
+++ code/shuffle.h
@@ -0,0 +1,16 @@
+#include "showworld.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#ifndef SHOWWORLD_H
+#define SHOWWORLD_H
+
+//void shuffle(AGENT *agents, unsigned int nagents) {
+	
+	
+	
+	
+//}
+
+#endif
